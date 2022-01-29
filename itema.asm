3d9c593cb40c044ce70e876d2791449bd05dce3b
Torkild U. Resheim <tur@itema.no>
2022-01-29 10:39:59 +0100

    Fix #18: Input acceleration

--- itema.asm
+++ itema.asm
@@ -1,107 +1,189 @@
 /*
-	Simple bouncing Itema logo
-	Copyright (c) 2020 Itema AS
+    Bouncing ball demo
 
-	Written by:
-	- Øystein Steimler, ofs@itema.no
-	- Torkild U. Resheim, tur@itema.no
-	- Morten Moen, mmo@itema.no
-	- Arve Moen, amo@itema.no
-	- Bjørn Leithe Karlsen, bka@itema.no
+    Copyright (c) 2020-2022 Itema AS
+
+    Written by:
+    - Øystein Steimler, ofs@itema.no
+    - Torkild U. Resheim, tur@itema.no
+    - Morten Moen, mmo@itema.no
+    - Arve Moen, amo@itema.no
+    - Bjørn Leithe Karlsen, bka@itema.no
 */
 
-* = $c000 "Main Program"
+* = $d000 "Main Program"
 
 // import our sprite library
-#import "spritedata.asm"
-#import "spritelib.asm"
-#import "music/music.asm"
-//
+#import "libSprite.asm"
+#import "libInput.asm"
+#import "libScreen.asm"
+
 BasicUpstart2(initialize)
-	
+
+.var music = LoadSid("music/Nightshift.sid")      //<- Here we load the sid file
+
 // Initialize
 initialize:
-	jsr $e544			// clear screen
+    jsr $e544           // clear screen
 
-	lda #%11111111		// enable all sprites
-	sta $d015
+    lda #$17            // activate character set 2
+    sta $d018
 
-	lda #$00			// disable xpand-y
-	sta $d017
+    lda #%00000111      // enable sprites
+    sta $d015
 
-	lda #$00			// set sprite/background priority
-	sta $d01b
-	
-	lda #$ff			// enable multicolor
-	sta $d01c
-	
-	lda #$00			// disable xpand-x
-	sta $d01d
-	
-	lda #$0f			// set sprite multicolor 1
-	sta $d025
-	lda #$0c			// set sprite multicolor 2
-	sta $d026
-	lda #$0a			// set sprite individual color
-	sta $d027
-	
-	lda #spriteData/64	// set sprite data pointer
-	sta $07f8			// sprite #1
-	sta $07f9			// sprite #2
-	sta $07fa			// sprite #3
-	sta $07fb			// sprite #4
-	sta $07fc			// sprite #5
-	sta $07fd			// sprite #6
-	sta $07fe			// sprite #7
-	sta $07ff			// sprite #8
-	
-	//jsr startMusic
+    lda #$00            // disable xpand-y
+    sta $d017
+
+    lda #$00            // set sprite/background priority
+    sta $d01b
+    
+//  lda #$ff            // enable multicolor
+//  sta $d01c
+    
+    lda #$00            // disable xpand-x
+    sta $d01d
+    
+//  lda #$0f            // set sprite multicolor 1
+//  sta $d025
+//  lda #$0c            // set sprite multicolor 2
+//  sta $d026
+    lda #$0a            // set sprite individual color
+    sta $d027
+    
+    lda #spriteData/64  // set sprite data pointer
+    sta $07f8           // sprite #1
+    sta $07f9           // sprite #2
+    sta $07fa           // sprite #3
+    sta $07fb           // sprite #4
+    sta $07fc           // sprite #5
+    sta $07fd           // sprite #6
+    sta $07fe           // sprite #7
+    sta $07ff           // sprite #8
+
+    // Set up some text for use when debugging
+    line1: .text "USE JOYSTICK 2"
+           .byte $ff
+           
+PRINT_SCREEN(line1, $0400)
+
+/*
+    Initialize IRQ
+*/
+jsr init_irq
 
 loop:
-	lda #$00
-	sta spriteindex
-	animation_loop:
-		jsr horizontal
-		jsr vertical	
-		jsr draw_sprite
-		inc spriteindex
-		lda spriteindex
-		cmp #$08
-		beq done
-		jmp animation_loop
-	done:
-		// Spend a few cycles doing nothing in order to get a smooth motion
-		ldy  #$10
-		ldx  #$01
-		delay:
-			dex
-			bne delay
-			dey
-			bne delay
 jmp loop
+
+player_input:
+    // Reset acceleration on both axis
+    lda #$00
+    jsr store_xa
+    jsr store_ya
+    
+    // Set acelleration according to joystick input
+    LIBINPUT_GET(GameportLeftMask)
+        bne inputRight
+        lda #$ff
+        jsr store_xa
+    inputRight:
+        LIBINPUT_GET(GameportRightMask)
+        bne inputUp
+        lda #$01
+        jsr store_xa
+    inputUp:
+        LIBINPUT_GET(GameportUpMask)
+        bne inputDown  
+        lda #$f8
+        jsr store_ya
+    inputDown:
+        LIBINPUT_GET(GameportDownMask)
+        bne inputEnd
+        lda #$01
+        jsr store_ya
+    inputEnd:   
+        rts 
+
+init_irq:
+
+    lda #$00
+    ldx #0
+    ldy #0
+    lda #music.startSong-1
+    jsr music.init
+
+    sei
+    lda #<irq_1
+    ldx #>irq_1
+    sta $0314
+    stx $0315           // Set interrupt addr    
+    lda #$7f
+    sta $dc0d           // Timer A off on cia1/kb
+    sta $dd0d           // Timer A off on cia2
+
+    lda #$81
+    sta $d01a           // Raster interrupts on
+    lda #$1b            // Screen ctrl: default
+    sta $d011
+
+    lda #$01
+    sta $d012           // Interrupt at line 0
+
+    lda $dc0d           // Clrflg (cia1)
+    lda $dd0d           // Clrflg (cia2)
+    asl $d019           // Clr interrupt flag (just in case)
+    cli
+    rts
+
+
+
+irq_1:
+    inc $d020
+    lda #$00
+    sta SpriteIndex
+    animation_loop:
+        jsr player_input
+        jsr move_horizontally
+        jsr move_vertically
+        jsr draw_sprite
+        inc SpriteIndex
+        lda SpriteIndex
+        cmp #$03
+        beq done
+        jmp animation_loop
+    done:
+    asl $d019
+    inc $d020
+    jsr music.play
+    dec $d020
+    dec $d020
+    jmp $ea81 //; set flag and end
+
+*=music.location "Music"
+.fill music.size, music.getData(i)              // <- Here we put the music in memory
 
 // -- Sprite Data --------------------------------------------------------------
 // Created using https://www.spritemate.com
 * = $2140 "Sprite Data"
 spriteData:
-.byte %00000000,%00101000,%00000000
-.byte %00000000,%10101010,%00000000
-.byte %00000001,%10101010,%01000000
-.byte %00000101,%10101010,%01010000
-.byte %00110111,%00101000,%11011100
-.byte %00010100,%00000000,%00010100
-.byte %11010100,%01010100,%00010111
-.byte %01011100,%01010100,%00110101
-.byte %01011100,%00010100,%00110101
-.byte %01011100,%00010100,%00110101
-.byte %01011100,%00010100,%00110101
-.byte %01011100,%00010100,%00110101
-.byte %01011100,%00010100,%00010101
-.byte %01011100,%00010100,%00010111
-.byte %01011100,%00010111,%11010100
-.byte %11010100,%00010101,%01011100
-.byte %00010100,%00010101,%01110000
-.byte %00110111,%00000000,%00000000
-.byte %00000101,%01111101,%00000000
-.byte %00001101,%01010101,%00000000
-.byte %00000011,%11011100,%00000000
+.byte %00000000,%00000000,%00000000
+.byte %00000000,%00000000,%00000000
+.byte %00000000,%00000000,%00000000
+.byte %00000000,%00000000,%00000000
+.byte %00000000,%00000000,%00000000
+.byte %00000000,%00011000,%00000000
+.byte %00000000,%01111110,%00000000
+.byte %00000000,%11111111,%00000000
+.byte %00000000,%11111111,%00000000
+.byte %00000001,%11111111,%10000000
+.byte %00000001,%11111111,%10000000
+.byte %00000000,%11111111,%00000000
+.byte %00000000,%11111111,%00000000
+.byte %00000000,%01111110,%00000000
+.byte %00000000,%00011000,%00000000
+.byte %00000000,%00000000,%00000000
+.byte %00000000,%00000000,%00000000
+.byte %00000000,%00000000,%00000000
+.byte %00000000,%00000000,%00000000
+.byte %00000000,%00000000,%00000000
+.byte %00000000,%00000000,%00000000
---
+++ libSprite.asm
@@ -0,0 +1,377 @@
+/*
+    Sprite handling library
+    Copyright (c) 2020-2022 Itema AS
+
+    This will simply bounce sprites between the four walls of the screen. Load
+    the current sprite number in to the A register and call the following
+    functions:
+
+    horizontal
+        to move horizontally
+
+    vertical
+        to move vertically
+
+    draw_sprite
+        to draw the sprite on it's new location
+
+    Written by:
+    - Øystein Steimler, ofs@itema.no
+    - Torkild U. Resheim, tur@itema.no
+    - Morten Moen, mmo@itema.no
+    - Arve Moen, amo@itema.no
+    - Bjørn Leithe Karlsen, bka@itema.no
+*/
+
+#importonce
+#import "libSpriteData.asm"
+
+SetTable:
+    .byte %00000001
+    .byte %00000010
+    .byte %00000100
+    .byte %00001000
+    .byte %00010000
+    .byte %00100000
+    .byte %01000000
+    .byte %10000000
+
+ClearTable:
+    .byte %11111110
+    .byte %11111101
+    .byte %11111011
+    .byte %11110111
+    .byte %11101111
+    .byte %11011111
+    .byte %10111111
+    .byte %01111111
+
+/*
+    Sprite box
+*/
+.const ScreenTopEdge    = $2c
+.const ScreenBottomEdge = $eb
+.const ScreenRightEdge  = $47
+.const ScreenLeftEdge   = $15
+
+.const Gravity          = $04
+.const VelocityLoss     = $04
+
+/*
+    A helper "variable" we will need on occasion
+*/
+temp:
+    .byte %00000000
+temp2:
+    .byte %00000000
+
+/*
+    Determine the offset of the sprite x-position address
+*/
+get_sprite_offset:
+    ldx SpriteIndex
+    lda #$00
+
+    get_sprite_offset_loop:
+        cpx #$00
+        beq got_sprite_offset
+        clc
+        adc #$2
+        dex
+        jmp get_sprite_offset_loop
+
+    got_sprite_offset:
+        rts
+
+draw_sprite:
+    // set vertical position
+    jsr get_sprite_offset
+    tay
+    jsr get_yl
+    sta $d001,y 
+
+    // set horizontal position
+    jsr get_sprite_offset
+    tay
+    jsr get_xl
+    sta $d000,y
+
+    // set horizontal position msb
+    jsr get_xm
+    cmp #$01
+    beq set_msb
+    
+    jsr get_xm
+    cmp #$00
+    beq clear_msb
+rts
+
+set_msb:
+    ldx SpriteIndex
+    lda $d010
+    ora SetTable,x
+    sta $d010
+rts
+
+clear_msb:
+    ldx SpriteIndex
+    lda $d010
+    and ClearTable,x
+    sta $d010
+rts
+
+////////////////////////////////////////////////////////////////////////////////
+
+/*
+    Apply the acceleration to the velocity, moving up. Once passing $FF (-1) the
+    direction will change to moving downwards. This transition causes some
+    velocity to be lost.
+*/
+bounce_up:
+    jsr get_yv
+    clc 
+    adc #Gravity                // Simulate gravity
+    jsr store_yv
+rts
+
+/*
+    Apply the acceleration to the velocity, moving down. Make sure that the
+    maximum value of #$7f is not exceeded because that would mean moving up.
+*/
+fall_down:
+    jsr get_yv
+    clc
+    adc #Gravity                // Simulate gravity
+    cmp #$80                // Never go negative
+    bpl fall_down_end   
+    jsr store_yv
+    fall_down_end:
+rts
+
+/*
+    Perform horizontal movement
+*/
+move_horizontally:
+    jsr h_acceleration      // Apply horizontal acceleration
+    jsr get_xv
+    clc
+    cmp #$00                // Compare with signed integer
+    bmi move_left           // Move left if value is negative
+    bpl move_right          // Move right if value is positive
+rts
+
+/*
+    Apply horizontal acceleration from input
+*/
+h_acceleration:
+    jsr get_xa
+    sta temp                // Store the new value in a variable
+    jsr get_xv
+    clc
+    adc temp                // Add acceleration to velocity
+    clv                     // Clear the overflow flag
+    bvs h_acceleration_end  // Do not store the value if the sign was flipped
+    jsr store_xv
+    h_acceleration_end:
+rts
+
+/*
+    Perform vertical movement
+*/
+move_vertically:
+    jsr v_acceleration      // Apply vertical acceleration
+    jsr get_yv
+    clc
+    cmp #$00                // Compare with signed integer
+    bmi up                  // Move up if value is negative
+    bpl move_down           // Move down if value is positive
+rts
+
+/*
+    Apply vertical acceleration from input along with gravity
+*/
+v_acceleration:
+    jsr get_ya  
+    sta temp                // Store the new value in a variable
+    jsr get_yv
+    clv                     // Clear the overflow flag
+    adc temp                // Add Y to A
+    bvs v_acceleration_end  // Do not store the value if the sign was flipped
+    jsr store_yv
+    // -- Apply gravity
+    cmp #$00                // Compare with signed integer
+    bpl fall_down           // Move down if value is positive
+    bmi bounce_up           // Move up if value is negative
+    v_acceleration_end:
+rts
+
+/*
+    Move current sprite left
+*/
+move_left:
+    jsr get_xv              // Get the X-velocity (which is negative)
+    eor #$ff                // Flip the sign so that we get a positive number
+    clc
+    adc #$01                // fix after flip
+    jsr shift_right         // Apply only the 5 MSB of velocity
+    sta temp                // Store the new value in a variable
+    jsr get_xl
+    sec
+    sbc temp                // Move left by the amount of velocity 
+    jsr store_xl
+    jsr get_xm
+    sbc #$00                // Subtract zero and borrow from lsb subtraction
+    jsr store_xm
+    jsr left_edge
+rts
+
+/*
+    Move current sprite right
+*/
+move_right:
+    jsr get_xv              // Get the X-velocity (a positive number)
+    jsr shift_right         // Apply only the 5 MSB of velocity
+    sta temp                // Store the value in a temporary variable
+    jsr get_xl
+    clc
+    adc temp                // Move right by the amount of velocity
+    jsr store_xl
+    jsr get_xm
+    adc #$00                // Add zero and carry from lsb addition
+    jsr store_xm
+    jsr right_edge
+rts 
+
+/*
+    Move current sprite upwards
+*/
+up:
+    jsr get_yv              // Get the Y-velocity (a negative number)
+    eor #$ff                // Flip the sign so that we get a positive number
+    clc
+    jsr shift_right         // Apply only the 5 MSB of velocity
+    sta temp                // Store the new value in a variable
+    jsr get_yl
+    sec                     // Set the carry flag
+    sbc temp                // Move up by the amount of velocity
+    jsr store_yl
+    cmp #ScreenTopEdge      // Is top of screen hit?
+    bcc change_to_move_down // Jump if less than $31
+rts
+
+/*
+    Move current sprite downwards
+*/
+move_down:
+    // Make sure we don't move below the bottom of the screen, so do not
+    // apply the velocity if the edge has already been hit.
+    jsr get_yl
+    cmp #ScreenBottomEdge   // Is bottom of screen hit?
+    bcs move_down_end
+    // OK go on and move the sprite
+    jsr get_yv              // Get the Y-velocity (a positive number)
+    jsr shift_right         // Apply only the 5 MSB of velocity
+    sta temp                // Store the value in a temporary variable
+    jsr get_yl
+    clc
+    adc temp                // Move down by the amount of velocity
+    jsr store_yl
+    cmp #ScreenBottomEdge   // Is bottom of screen hit?
+    bcs change_to_move_up   // If so change directions
+    move_down_end:
+rts
+
+/*
+    Flip the sign on the vertical velocity and acceleration
+*/
+change_to_move_up:
+    jsr get_yv              // Change the direction of the velocity
+    clc
+    sbc #VelocityLoss       // Reduce velocity  
+    eor #$ff                // Flip the sign
+    jsr store_yv
+rts
+
+change_to_move_down:
+    jsr get_yv              // Change the direction of the velocity
+    clc
+    adc #VelocityLoss       // Reduce velocity  
+    eor #$ff                // Flip the sign
+    jsr store_yv
+rts
+
+/*
+    Start moving from left to right.
+*/
+change_to_move_right:
+    jsr get_xv              // Change the direction of the velocity
+    clc
+    adc #VelocityLoss       // Reduce velocity
+    eor #$ff                // Flip the sign
+    jsr store_xv
+rts
+
+/*
+    Start moving from right to left.
+*/
+change_to_move_left:
+    jsr get_xv              // Change the direction of the velocity
+    clc
+    sbc #VelocityLoss       // Reduce velocity
+    eor #$ff                // Flip the sign
+    jsr store_xv
+rts
+
+/*
+    Determine whether or not the current sprite is at the right edge of the
+    screen.
+*/
+right_edge:
+    jsr get_xm
+    clc
+    cmp #$01                // Compare with #01 (over fold)
+    beq over_fold
+rts
+
+/*
+    Change direction and start moving leftwards
+*/
+over_fold:
+    jsr get_xl
+    clc
+    cmp #ScreenRightEdge
+    bcs change_to_move_left
+rts
+
+/*
+    Determine whether or not the current sprite is at the left edge of the 
+    screen
+*/
+left_edge:
+    jsr get_xm
+    clc
+    cmp #$01                // Compare with #01 (at fold)
+    bne at_left_edge
+rts
+
+/*
+    Change direction and start moving rightwards
+*/
+at_left_edge:
+    jsr get_xl
+    clc
+    cmp #ScreenLeftEdge
+    bcc change_to_move_right
+rts
+
+shift_right:
+    clc
+    ror
+    clc
+    ror
+    clc
+    ror
+    clc
+    ror
+    clc
+rts
