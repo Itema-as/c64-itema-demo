c4efbd358cb5287656f4baa17de089ffc3ff57e4
Torkild U. Resheim <tur@itema.no>
2023-01-28 10:00:49 +0100

    Minor adjustments

--- itema.asm
+++ itema.asm
@@ -75,11 +75,11 @@ initialize:
 */
 
 lda $d018
-ora #$0e       // set chars location to $3800 for displaying the custom font
-sta $d018      // Bits 1-3 ($400+512bytes * low nibble value) of $d018 sets char location
-                          // $400 + $200*$0E = $3800
+ora #%00001110 // Set chars location to $3800 for displaying the custom font
+sta $d018      // Bits 1-3 ($0400 + 512 bytes * low nibble value) of $D018 sets char location
+               // $400 + $200*$0E = $3800
 lda $d016      // turn off multicolor for characters
-and #$ef       // by clearing Bit#4 of $D016
+and #%11101111 // by clearing bit #4 of $D016
 sta $d016
 
 /*
